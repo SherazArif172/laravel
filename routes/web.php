86bc2576fee20e724cfee0ab7aa28a5fdf30e11f
Sheraz Arif <sherazarifofficial@gmail.com>
2024-09-13 18:27:54 +0500

    added something

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('new');
 });
+Route::get('/about/{name}', function ($name) {
+    return view('about',['name'=>$name]);
+});
